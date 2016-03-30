2cfd9b3916bd08dadfd4588d3223c3c1180c562a
4AHIF-Roszner <ros15009@spengergasse.at>
2016-03-30 13:37:20 +0200

    Created project

---
+++ ConnectMe!/MongoDBLib/MongoDBWrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDBLib
+{
+    class MongoDBWrapper
+    {
+        private IMongoClient client;
+        private IMongoDatabase db;
+
+        public MongoDBWrapper()
+        {
+            var settings = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress(
+                 Properties.Settings.Default.MongoDBHost,
+                 Properties.Settings.Default.MongoDBPort)
+            };
+
+            this.client = new MongoClient(settings);
+            this.db = client.GetDatabase(
+              Properties.Settings.Default.MongoDBName);
+        }
+
+        public Boolean Insert()
+        {
+            try
+            {
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+
+        }
+        public Boolean Delete()
+        {
+            try
+            {
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
